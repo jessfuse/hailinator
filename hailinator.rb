40a35822fe8ddb84214603903cc81720edf45f72
MakerSquare Student <student@challenge-8.att.net>
2013-06-19 10:27:46 -0500

    authenticated twitter

--- hailinator.rb
+++ hailinator.rb
@@ -1,1 +1,11 @@
-require "twitter" 
+require "twitter"
+
+Twitter.configure do |config|
+  config.consumer_key = "uvkCuFf2d63ZmEodWSXc6Q"
+  config.consumer_secret = "qBDxBFnqULp4TjhfdBfX24BmbvfcoPmVkMpC3YMiQ"
+  config.oauth_token = "499825106-e1TYfau2cqiue3GS2hCPZLpSNljYmfciUPzZAXM"
+  config.oauth_token_secret = "OY978g3x5r1e4BwSb4x3ixt8igppSaq0cpFSXTyK2I"
+end
+
+#hack 
+Twitter.home_timeline
